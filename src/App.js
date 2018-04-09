57ce8e72927d1ae3cb3cf6409f3f3244a02cc141
blue <blue@tigrison.com>
2018-04-09 16:07:01 +0900

    더보기(페이지API) 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 
+const DEFAULT_HPP = '100';
+
 const DEFAULT_QUERY = 'redux';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
-
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage='
 const isSearched = searchTerm => 
       item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -26,10 +29,18 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({result});
+    const {hits, page} = result;
+    const oldHits = page !== 0 ? this.state.result.hits : [];
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    this.setState({
+      result : {hits : updatedHits, page}
+    });
   }
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => {
         return this.setSearchTopStories(result)
@@ -64,6 +75,8 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
+
+    const page = (result && result.page) || 0;
     // if(!result) {return null;}
     return (
       <div className="page">
@@ -80,6 +93,11 @@ class App extends Component {
               list={result.hits}
               onDismiss={this.onDismiss} />
           }
+          <div className="interactions">
+            <Button onClick={()=> this.fetchSearchTopStories(searchTerm, page+1)}>
+              More
+            </Button>
+          </div>
           
         </div>
       </div>

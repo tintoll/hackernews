3c5aef4d7ae3ccff00d0cd91f00f00718a4141b8
blue <blue@tigrison.com>
2018-04-12 16:19:04 +0900

    컴포넌트 리팩토링

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import './index.css';
+import {
+  DEFAULT_HPP,
+  DEFAULT_QUERY,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP,
+} from '../../constants';
+import Table from '../Table';
+import Button from '../Button';
+import Search from '../Search';
 
-
-const DEFAULT_HPP = '100';
-
-const DEFAULT_QUERY = 'redux';
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage='
- // const isSearched = searchTerm => 
- //     item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+// const isSearched = searchTerm => 
+//     item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   _isMounted = false;
@@ -22,10 +25,10 @@ class App extends Component {
     // 객체 초기화 
     // list : list
     this.state = {
-      results : null,
-      searchKey : '',
-      searchTerm : DEFAULT_QUERY,
-      error : null
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+      error: null
     }
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
@@ -34,22 +37,22 @@ class App extends Component {
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
 
-  needsToSearchTopStories(searchTerm){
+  needsToSearchTopStories(searchTerm) {
     return !this.state.results[searchTerm];
-  } 
+  }
   setSearchTopStories(result) {
-    const {hits, page} = result;
-    const {searchKey, results} = this.state;
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
 
-    const oldHits = results && results[searchKey] ? results[searchKey] : [];
+    const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
     const updatedHits = [
       ...oldHits,
       ...hits
     ];
     this.setState({
-      results : {
+      results: {
         ...results,
-        [searchKey] : {hits : updatedHits, page}
+        [searchKey]: { hits: updatedHits, page }
       }
     });
   }
@@ -67,14 +70,14 @@ class App extends Component {
       .then(result => {
         return this._isMounted && this.setSearchTopStories(result.data)
       })
-      .catch(error => this._isMounted && this.setState({error}));
+      .catch(error => this._isMounted && this.setState({ error }));
   }
   componentDidMount() {
     this._isMounted = true;
     const { searchTerm } = this.state;
     // 캐시내 현재 결과를 가리키는 포인터 역할 
     this.setState({
-      searchKey : searchTerm
+      searchKey: searchTerm
     });
     this.fetchSearchTopStories(searchTerm);
   }
@@ -84,23 +87,24 @@ class App extends Component {
 
   onDismiss(id) {
 
-    const {searchKey, results} = this.state;
-    const {hits, page} = results[searchKey];
-    
-    const isNotId = item => item.objectID !== id; 
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
+    const isNotId = item => item.objectID !== id;
     const updatedHits = hits.filter(isNotId);
     //  hits기사만 업데이트 하기 위해서 Object.assign을 사용 
     // Object.assign()을 전개연산자로 대체할수있음.
     this.setState({
       // result: Object.assign({}, this.state.result, {hists:updatedHits})
-      results : {
-        ...results, 
-        [searchKey]:{hits:updatedHits, page}}
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
     });
   }
   onSearchChange(event) {
     this.setState({
-      searchTerm : event.target.value
+      searchTerm: event.target.value
     });
   }
   onSearchSubmit(event) {
@@ -108,9 +112,9 @@ class App extends Component {
     this.setState({
       searchKey: searchTerm
     });
-    if(this.needsToSearchTopStories(searchTerm)) {
+    if (this.needsToSearchTopStories(searchTerm)) {
       this.fetchSearchTopStories(searchTerm);
-    } 
+    }
     event.preventDefault();
   }
 
@@ -128,76 +132,34 @@ class App extends Component {
     return (
       <div className="page">
         <div className="interactions">
-          <Search 
+          <Search
             value={searchTerm}
             onChange={this.onSearchChange}
             onSubmit={this.onSearchSubmit}>
             Search
           </Search>
           {
-            error ? 
-            <div className="interactions">
-              <p>Something went wrong.</p>
-            </div> 
-            :
-            <Table
-              list={list}
-              onDismiss={this.onDismiss} />
+            error ?
+              <div className="interactions">
+                <p>Something went wrong.</p>
+              </div>
+              :
+              <Table
+                list={list}
+                onDismiss={this.onDismiss} />
           }
-          
-          
+
+
           <div className="interactions">
-            <Button onClick={()=> this.fetchSearchTopStories(searchKey, page+1)}>
+            <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
               More
             </Button>
           </div>
-          
+
         </div>
       </div>
     );
   }
-}
-
-const Search = ({ value, onSubmit, onChange, children }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <input type="text"
-        value={value}
-        onChange={onChange} />
-      <button type="submit">{children}</button>  
-    </form>
-  )
-}
-const Table = ({ list, onDismiss }) => {
-  return (
-    <div className="table">
-      {list.map(item =>
-        <div key={item.objectID} className="table-row">
-          <span style={{ width: '40%' }}><a href={item.url}>{item.title}</a></span>
-          <span style={{ width: '30%' }}>{item.author}</span>
-          <span style={{ width: '10%' }}>{item.num_comments}</span>
-          <span style={{ width: '10%' }}>{item.points}</span>
-          <span style={{ width: '10%' }}>
-            <Button
-              onClick={() => onDismiss(item.objectID)}
-              className="button-inline">
-              Dismiss
-              </Button>
-          </span>   
-        </div>
-      )}
-    </div>
-  )
-}
-
-const Button = ({ onClick, className = '',children }) => {
-  return (
-    <button onClick={onClick}
-      className={className}
-      type="button">
-      {children}
-    </button>
-  )
 }
 
 export default App;
